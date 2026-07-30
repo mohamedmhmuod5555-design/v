5da3b350f4cf779b66f604fafbae8a4474b6336b
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-30 15:34:03 -0700

    Update the best thing 5.py

--- the best thing 5.py
+++ the best thing 5.py
@@ -36,13 +36,13 @@ st.write(num1,sign,num2)
 number=st.number_input("ادخل النتيجه ")
 if st.button("تأكيد التخمين "):
   st.session_state.count += 1
- if number == sc:
-   st.session_state.num += 1
-   st.session_state.feed="correct"
-  else:
-    st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت : {sc}")
-    st.session_state.num =0
-  st.session_state.feed="false" 
+   if number == sc:
+     st.session_state.num += 1
+    st.session_state.feed="correct"
+    else:
+     st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت : {sc}")
+     st.session_state.num =0
+     st.session_state.feed="false" 
 if st.session_state.feed=="correct":
   st.success("انك اسطوره يا عبقري الرياضه ")
   del st.session_state.num1
