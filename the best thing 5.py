541fce903fc570545f1cc12d5e9d041986d92034
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-30 15:45:13 -0700

    Update the best thing 5.py

--- the best thing 5.py
+++ the best thing 5.py
@@ -43,20 +43,20 @@ if st.button("تأكيد التخمين "):
      st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت : {sc}")
      st.session_state.num =0
      st.session_state.feed="false" 
-   if st.session_state.feed=="correct":
+    if st.session_state.feed=="correct":
      st.success("انك اسطوره يا عبقري الرياضه ")
      del st.session_state.num1
      del st.session_state.num2
      del st.session_state.sign
      time.sleep(1)
      st.rerun()
-  if st.session_state.feed=="false":
-   st.success("للاسف اجابتك خطء انا حزين لان مستواك كان جيد  ")
-   del st.session_state.num1
-   del st.session_state.num2
-   del st.session_state.sign 
-   time.sleep(1)
-   st.rerun()   
+    if st.session_state.feed=="false":
+    st.success("للاسف اجابتك خطء انا حزين لان مستواك كان جيد  ")
+    del st.session_state.num1
+    del st.session_state.num2
+    del st.session_state.sign 
+    time.sleep(1)
+    st.rerun()   
 
 if st.session_state.num > 0 and st.session_state.num % 10 == 0:
   st.success("انت بطل! تحدي صديقك انه بالطبع لن يستطيع ان يصل لمستواك  ")
